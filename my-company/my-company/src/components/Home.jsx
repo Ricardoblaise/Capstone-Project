dafddc5616d9ea6dd919d970961819d6a2ff11b0
Blaise <ricardoblaise120@gmail.com>
2024-08-23 11:43:17 +0200

    ✅feature: my-company codes updated

---
+++ my-company/my-company/src/components/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to Our Company</h1>
+      <p>We are dedicated to delivering excellence in all our services.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit earum laboriosam?</p>
+    </div>
+  );
+}
+
+export default Home;
