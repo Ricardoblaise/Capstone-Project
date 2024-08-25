01677ac6da2443112ebb324682d2603bfe262949
Blaise <ricardoblaise120@gmail.com>
2024-08-25 12:12:54 +0200

    ✅feature:all codes modified

--- recipe-sharing-app/recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import RecipeList from "./components/RecipeList"
 import AddRecipeForm from "./components/AddRecipeForm"
 
@@ -5,10 +6,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipe />
+        <RecipeList />
+    </div>
+  );
+};
 
 export default App
